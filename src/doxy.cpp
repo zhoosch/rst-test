e6d008132c9c1fe7fd1ada187d2a88236cdac3e2
zhoosch <zhoosch@aol.com>
2020-05-14 18:43:34 +0200

    add doxy test

---
+++ src/doxy.cpp
@@ -0,0 +1,13 @@
+/***************************************************************************//**
+ * A brief history of Doxygen-style banner comments.
+ *
+ * This is a Doxygen-style C-style "banner" comment. It starts with a "normal"
+ * comment and is then converted to a "special" comment block near the end of
+ * the first line. It is written this way to be more "visible" to developers
+ * who are reading the source code.
+ * This style of commenting behaves poorly with clang-format.
+ *
+ * @param theory Even if there is only one possible unified theory. it is just a
+ *               set of rules and equations.
+ ******************************************************************************/
+void doxygenBanner( int theory );
